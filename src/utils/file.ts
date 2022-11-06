927ee530c1e579b68fd141eb9e5d2c3963549c2e
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2022-11-06 11:17:01 -0300

    feat: add file utils.

---
+++ src/utils/file.ts
@@ -0,0 +1,39 @@
+import { extname } from "node:path";
+import { unlink, promises } from "node:fs";
+import { errors } from "../contants/error";
+
+export function checkSameExtension(
+  files: Express.Multer.File[] | undefined,
+  extensions: string[]
+): boolean {
+  let hasDuplicated = false;
+  if (!files) {
+    return true;
+  }
+
+  extensions.forEach((extension) => {
+    if (hasDuplicated) {
+      return;
+    }
+    if (files.every((el) => extname(el.originalname) === extension)) {
+      hasDuplicated = true;
+      return;
+    }
+    hasDuplicated = false;
+  });
+
+  return hasDuplicated;
+}
+
+export async function unlinkfile(path: string): Promise<void> {
+  try {
+    await promises.stat(path);
+    unlink(path, (err) => {
+      if (err) {
+        throw new Error(errors.REMOVE_FILE);
+      }
+    });
+  } catch (err) {
+    throw new Error(errors.INVALID_PATH);
+  }
+}
